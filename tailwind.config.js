b0a60deb246fc3a3cf2676421971ed32fcffaff8
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-10-10 09:09:54 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'input-primary': '#FA9874',
+        'input-black': '#101928',
+        'input-grey': '#D0D5DD',
+        'input-button': '#EB5017',
+        'input-links': '#CC400C',
+      },
+    },
+  },
+  plugins: [],
+};
